b15b86592e6ffdb441a763277c4eb6c403486a68
Lebowskidev <andriy.auditore@gmail.com>
2016-07-15 13:23:27 +0300

    Зчитування всіх даних з бази даних , для полегшення зміни на сторінці.
    Signed-off-by: Lebowskidev <andriy.auditore@gmail.com>

--- views/site/pi.php
+++ views/site/pi.php
@@ -17,13 +17,14 @@ use yii\widgets\LinkPager;
 
         <div class="list-group">
             <div class="list-group-item row">
-                <h2 class="list-group-item-heading">Початок</h2>
-        <div class="col-md-3">
-                <img src="files/Begin.jpg" class="img-rounded">
+                <?php foreach ($dats as $query): if($query->pi == 0000000001 ){ ?>
+                <h2 class="list-group-item-heading"><?= Html::encode("{$query->nam}"); ?></h2>
+                <div class="col-md-3">
+             <img src=" <?= Html::encode("{$query->img}"); ?>" class="img-rounded">
             </div>
         <div class="col-md-3 col-md-offset-3 ">
                 <p class="text-justify">
-                    <?php foreach ($dats as $query): if($query->pi == 0000000001 ){ ?>
+
                         <?= Html::encode("{$query->dat}") ?>
 
 
@@ -42,14 +43,14 @@ use yii\widgets\LinkPager;
             </div>
             <br>
             <div class="list-group-item row">
-                <h2 class="list-group-item-heading">Легенда Г'ю Гласса</h2>
+                <?php foreach ($dats as $query): if($query->pi == 0000000002 ){ ?>
+                <h2 class="list-group-item-heading"><?= Html::encode("{$query->nam}") ?></h2>
                 <div class="col-md-3">
-                <img src="files/Legend.jpg" class="img-rounded">
+                    <img src="<?= Html::encode("{$query->img}") ?>" class="img-rounded">
                     </div>
                 <div class="col-md-3 col-md-offset-3 ">
-                <p class="text-justify"> <?php foreach ($dats as $query): if($query->pi == 0000000002 ){ ?>
+                <p class="text-justify">
                         <?= Html::encode("{$query->dat}") ?>
-
                 </p>
                     </div>
                 <div class="col-md-4 col-md-offset-4">
@@ -65,12 +66,13 @@ use yii\widgets\LinkPager;
             </div>
             <br>
             <div class="list-group-item row">
-                <h2 class="list-group-item-heading">Драйв</h2>
+                <?php foreach ($dats as $query): if($query->pi == 0000000003 ){ ?>
+                <h2 class="list-group-item-heading"><?= Html::encode("{$query->nam}") ?></h2>
                 <div class="col-md-3">
-                <img src="files/Drive.jpg" class="img-rounded">
+                <img src="<?= Html::encode("{$query->img}") ?>" class="img-rounded">
                     </div>
                 <div class="col-md-3 col-md-offset-3 ">
-                <p class="text-justify"> <?php foreach ($dats as $query): if($query->pi == 0000000003 ){ ?>
+                <p class="text-justify">
                         <?= Html::encode("{$query->dat}") ?>
 
                     </div>
